ce6def3e93b4b0b24f4cbd8e5e0f49eeba580459
unknown <abhaymoon99@gmail.com>
2021-05-11 23:03:55 +0530

    updating in database

--- src/Database/database.js
+++ src/Database/database.js
@@ -1,5 +1,8 @@
 require('dotenv').config({path:"../.env"})
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // const databasename = "mongodb+srv://abhaymoon99gmailcom:Birthday99@cluster0.2i0ac.mongodb.net/RegisterForm?retryWrites=true&w=majority";
 const databasename = process.env.DATABASE_NAME;
